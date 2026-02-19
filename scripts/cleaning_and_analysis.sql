40d04d2e40c7b580395234cf24605dd7143767a0
Mitul Sharma <sharma.mitul2004@gmail.com>
2026-02-19 20:44:54 +0530

    Update cleaning_and_analysis.sql

--- scripts/cleaning_and_analysis.sql
+++ scripts/cleaning_and_analysis.sql
@@ -85,3 +85,10 @@ SELECT
     SUM(downloads) AS total_downloads
 FROM apps_data
 GROUP BY CASE WHEN price = 0 THEN 'Free' ELSE 'Paid' END;
+
+SELECT 
+    app_type, 
+    COUNT(*) AS total_apps
+FROM apps_data
+WHERE app_type IS NOT NULL
+GROUP BY app_type;
